b395ffe6d72783feea9436c47bcb7ce6726281bb
David Mansy <david.mansy@icloud.com>
2018-07-17 08:55:41 +0200

    First commit

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+import todos from './todos';
+import goals from './goals';
+import loading from './loading';
+
+import { combineReducers } from 'redux';
+
+export default combineReducers({ todos, goals, loading });
